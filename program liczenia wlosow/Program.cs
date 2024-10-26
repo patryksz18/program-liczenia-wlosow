35ee0bd185480ccc11130636e5bb3adebb720c6c
patryksz18 <patrykkarol07@gmail.com>
2024-10-26 13:42:10 +0200

    stworzenie domylsnych wartocsi dla obliczen oraz stworzenie zmiennych

--- program liczenia wlosow/Program.cs
+++ program liczenia wlosow/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+namespace SzacowanieWlosowNaGlowie
+{
+    public class Glowa
+    {
+        // stale dla domylsnych wartosci obliczen
+        private const double SredniaLiczbaWlosow = 100000;
+        private const double DomyslnyObwodGlowy = 56.0; // w cm
+        private const double DomyslnaWysokoscCzola = 15.0; // w cm
+        // prywatne pola
+        private double obwodGlowy;
+        private double wysokoscCzolla;
+        private double gestoscWlosow; // ilosc na cm2
+        private double powierzchniaGlowy;
+        private double liczbaWlosow;
+
+    }
+}
